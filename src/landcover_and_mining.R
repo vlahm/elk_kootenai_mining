88cdcf81b9e5996c70817b0a1b68774d94805772
Mike Vlah <vlahm13@gmail.com>
2026-02-24 08:48:59 -0700

    restarting major basin landcover change analysis with huc8s

--- src/landcover_and_mining.R
+++ src/landcover_and_mining.R
@@ -4,7 +4,7 @@ library(sf)
 library(lubridate)
 library(ggplot2)
 
-glc_smry <- read_csv('data/glc_watershed_summary.csv') %>% 
-  rename(impervious = urban)
+kootenai_basin <- st_read('data/elk_kootenai_basin/h8/ek_h8.shp')
 
-
+elk_basin <- filter(kootenai_basin, huc8 == 17010106)
+kootenay_basin <- filter(kootenai_basin, huc8 %in% c(17010107, 17010108))
